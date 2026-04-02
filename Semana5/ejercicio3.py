e4cd8d1f77f8b595f079fab4043caea74cd01052
pablolazo1 <pablolzx99x@gmail.com>
2026-04-02 04:56:40 -0300

    Update ejercicio3.py

--- Semana5/ejercicio3.py
+++ Semana5/ejercicio3.py
@@ -6,8 +6,15 @@ Debe implementar una versión iterativa y una recursiva.
 """
 
 def factorial_ciclo(n):
-    pass
-
+    resultado=1
+    while n>1:
+        resultado=resultado*n
+        n=n-1
+    return resultado
 
 def factorial_recursivo(n):
-    pass
+    if n==o or n==1:
+        return 1
+    else:
+        return n*factorial_recursivo(n-1)
+    
